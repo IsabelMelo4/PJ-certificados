2d8e97b118ede9f32d8f7e01bcc8492e2c2a6e18
IsabelMelo4 <isabelmelo630@gmail.com>
2025-03-15 18:57:58 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,59 @@
+document.getElementById("caixa").addEventListener("input", function () {
+    const nome = this.value.trim();
+    const nomeElemento = document.getElementById("nome");
+
+    if (nome === "") {
+        nomeElemento.textContent = "Seu nome aqui";
+        nomeElemento.style.fontSize = "50px"; 
+        return;
+    }
+
+    nomeElemento.textContent = nome;
+
+    let fontSize = 50;
+    nomeElemento.style.fontSize = fontSize + "px";
+
+    while (nomeElemento.scrollWidth > 600 && fontSize > 10) { 
+        fontSize--;
+        nomeElemento.style.fontSize = fontSize + "px";
+    }
+});
+
+function gerarCertificado() {
+    const nome = document.getElementById("caixa").value.trim();
+    
+    if (nome === "") {
+        alert("Por favor, digite seu nome.");
+        return;
+    }
+
+    const { jsPDF } = window.jspdf;
+    const doc = new jsPDF("landscape");
+
+    const img = new Image();
+    img.src = "assets/Certificado enccri.png";
+
+    img.onload = function () {
+        doc.addImage(img, "PNG", 0, 0, 297, 210); 
+
+        doc.setFont("helvetica", "bold");
+        let fontSize = 40; 
+        doc.setFontSize(fontSize);
+        doc.setTextColor(82, 113, 255); 
+    
+        while (doc.getTextWidth(nome) > 180 && fontSize > 10) {
+            fontSize--;
+            doc.setFontSize(fontSize);
+        }
+
+       
+        doc.text(nome, 200, 90, { align: "center" });
+        
+        doc.autoPrint();
+        window.open(doc.output("bloburl"), "_blank");
+    };
+
+    img.onerror = function () {
+        alert("Erro ao carregar a imagem do certificado.");
+    };
+}
